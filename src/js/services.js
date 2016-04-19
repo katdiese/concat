a7852c8589000de3092b7e3c2ca823fdabcc2145
Kathy Dieser <katdiese@gmail.com>
2016-04-19 16:18:03 -0600

    included api call for all users

--- src/js/services.js
+++ src/js/services.js
@@ -9,10 +9,16 @@
     userService.$inject = ['$http'];
 
     function userService($http) {
-      var dataApi = 'https://galvanize-student-apis.herokuapp.com/gdating/'
+      var dataApi = 'https://galvanize-student-apis.herokuapp.com/gdating'
       return {
         getAll: function(limit, offset) {
-          return $http.get('dataApi' + members + '?limit=' + limit + '&offset=' + offset);
+          return $http.get(dataApi + '/members' + '?limit=' + limit + '&offset=' + offset)
+          .then(function(res) {
+            console.log(res);
+          })
+          .catch(function(err) {
+            return err
+          });
         }
       }
     }
