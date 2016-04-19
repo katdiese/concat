f2d0ee65ed9436b03fe2997b0610f2646580e745
Kathy Dieser <katdiese@gmail.com>
2016-04-19 16:17:20 -0600

    included footer directive tag

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,7 @@ angular.module('concatApp')
       .when('/', {
         template: '<landing-page></landing-page>'
       })
+      .when('/users', {
+        template: '<main-nav></main-nav><all-users></all-users><footer-content></footer-content>'
+      })
   })
