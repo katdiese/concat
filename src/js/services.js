40fe6d1affda82a63d8acae6335be20ee972cab3
Kathy Dieser <katdiese@gmail.com>
2016-04-19 16:59:51 -0600

    return response instead of console.logging it

--- src/js/services.js
+++ src/js/services.js
@@ -14,10 +14,10 @@
         getAll: function(limit, offset) {
           return $http.get(dataApi + '/members' + '?limit=' + limit + '&offset=' + offset)
           .then(function(res) {
-            console.log(res);
+            return res;
           })
           .catch(function(err) {
-            return err
+            return err;
           });
         }
       }
