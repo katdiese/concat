4493cbd7ac028d88d890fbba53907559a013738f
Kathy Dieser <katdiese@gmail.com>
2016-04-19 14:36:31 -0600

    wrote service to make api call to members

---
+++ src/js/services.js
@@ -0,0 +1,20 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('concatApp')
+    .service('userService', userService);
+
+    userService.$inject = ['$http'];
+
+    function userService($http) {
+      var dataApi = 'https://galvanize-student-apis.herokuapp.com/gdating/'
+      return {
+        getAll: function(limit, offset) {
+          return $http.get('dataApi' + members + '?limit=' + limit + '&offset=' + offset);
+        }
+      }
+    }
+
+})();
