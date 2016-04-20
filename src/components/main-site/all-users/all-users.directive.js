e9236167987059875598357094c3883d5ff39c52
Kathy Dieser <katdiese@gmail.com>
2016-04-19 20:17:51 -0600

    call to api to get one user

--- src/components/main-site/all-users/all-users.directive.js
+++ src/components/main-site/all-users/all-users.directive.js
@@ -12,13 +12,19 @@
         templateUrl: 'components/main-site/all-users/all-users.html.html',
         controller:
           function($scope, userService) {
-            $scope.message = "You got to all users!"
             $scope.currentOffset = 0;
             $scope.getUsers = function(limit, offset) {
               userService.getAll(limit, offset)
               .then(function(users) {
                 console.log(users);
                 $scope.users = users.data.data;
+              })
+            }
+            $scope.getOneUser = function(id) {
+              userService.getOne(id)
+              .then(function(user) {
+                console.log(user);
+                $scope.oneUser = user.data.data;
               })
             }
             $scope.paginate = function(direction) {
--- src/js/services.js
+++ src/js/services.js
@@ -12,7 +12,16 @@
       var dataApi = 'https://galvanize-student-apis.herokuapp.com/gdating'
       return {
         getAll: function(limit, offset) {
-          return $http.get(dataApi + '/members' + '?limit=' + limit + '&offset=' + offset)
+          return $http.get(dataApi + '/members?limit=' + limit + '&offset=' + offset)
+          .then(function(res) {
+            return res;
+          })
+          .catch(function(err) {
+            return err;
+          });
+        },
+        getOne: function(id) {
+          return $http.get(dataApi + '/members/' + id)
           .then(function(res) {
             return res;
           })
