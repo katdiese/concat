af29099a19f09ff713c5c7d76e6149fa3d3b2957
Kathy Dieser <katdiese@gmail.com>
2016-04-19 19:07:11 -0600

    added footer to landing page

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ angular.module('concatApp')
   .config(function($routeProvider) {
     $routeProvider
       .when('/', {
-        template: '<landing-page></landing-page>'
+        template: '<landing-page></landing-page><footer-content></footer-content>'
       })
       .when('/users', {
         template: '<main-nav></main-nav><all-users></all-users><footer-content></footer-content>'
