3de2d2ada6dec6853588a5846a8d9fe3b4b8d9df
Kathy Dieser <katdiese@gmail.com>
2016-04-21 14:44:38 -0600

    attempts at updating interests

--- src/components/main-site/single-profile/single-profile.directive.js
+++ src/components/main-site/single-profile/single-profile.directive.js
@@ -18,7 +18,36 @@
                 $scope.oneUser = user.data.data;
               })
             }
+            $scope.check0 = "";
+            $scope.check1 = "";
+            $scope.check2 = "";
+            $scope.check3 = "";
+
+            $scope.newInterests = [];
+
+            $scope.$watchGroup(['check0', 'check1', 'check2', 'check3'],
+            function (vals) {
+              // vals.forEach(function(el) {
+              //   if(el !== "") {arr.push(el);}
+              //   console.log(arr);
+              // })
+              $scope.interestArr = vals;
+              // console.log($scope.oneUser.interestedIn);
+            });
+
+            $scope.oneUser.interestedIn = [1,2];
+
             $scope.updateUser = function(data) {
+              // data.interestedIn = [];
+              // console.log('old:::', data.interestedIn);
+              // $scope.interestArr.forEach(function(el) {
+              //   if(el !== "") {
+              //     data.interestedIn.push(parseInt(el));
+              //   }
+              // })
+              // data.interestedIn = $scope.newInterest;
+              // console.log('new:::', data.interestedIn);
+              // $scope.oneUser.interestedIn = $scope.newInterests;
               userService.updateOne($rootScope.currentUser.id, data)
               .then(function(user) {
                 console.log(user);
