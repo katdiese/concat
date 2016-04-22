22c3c2f7ce93ddbddbef1cf7917a2e38ec8b442f
Kathy Dieser <katdiese@gmail.com>
2016-04-22 11:44:55 -0600

    added matches filter

--- src/components/main-site/all-users/all-users.directive.js
+++ src/components/main-site/all-users/all-users.directive.js
@@ -16,28 +16,33 @@
             $scope.displayMore = function() {
               $scope.currentDisplay += 10;
             }
-            $scope.getUsers = function() {
-              userService.getAll()
-              .then(function(users) {
-                $scope.users = users.data.data;
-              })
-            }
             $scope.getOneUser = function(id) {
               userService.getOne(id)
               .then(function(user) {
                 $scope.oneUser = user.data.data;
               })
             }
-            // $scope.paginate = function(direction) {
-            //   if(direction === 'forward') {
-            //     $scope.currentOffset += 11;
-            //   } else if (direction === 'backward') {
-            //     $scope.currentOffset -= 11;
-            //   }
-            //     $scope.getUsers();
-            //   }
+            $scope.getUsers = function() {
+              userService.getAll()
+              .then(function(users) {
+                $scope.allUsers = users.data.data;
+                $scope.users = users.data.data;
+              })
+            }
             $scope.getUsers();
-
+            $scope.popularFilter = function() {
+              $scope.users = $scope.allUsers.filter(function(el) {
+                return el._matches.length > 8
+              })
+            }
+            $scope.nearbyFilter
+            $scope.matchesFilter = function() {
+              $scope.users = $scope.allUsers.filter(function(el) {
+              return el._matches.filter(function(arr) {
+                return arr === '5719234249f05f11000fdb6f';
+              }).length > 0;
+            })
+            }
           }
       }
     }
